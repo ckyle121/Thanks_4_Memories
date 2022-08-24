060f839c6a719151433f97d94ff0826087f31998
ckyle121 <carolinekyle21@gmail.com>
2022-08-24 10:25:55 -0500

    skeleton front end components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import Posts from './components/Posts/Posts.js';
+import Form from "./components/Form/Form.js";
+
+import memories from './images/memories.png';
 
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Memories</Typography>
+                <img src={memories} alt="memories" heigth="60"></img>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7} md>
+                            <Posts /> 
+                        </Grid>
+                        <Grid item xs={12} sm={4} md>
+                            <Form /> 
+                        </Grid>
+                    </Grid>
+                </Container>
+
+            </Grow>
+        </Container>
     );
 }
 
