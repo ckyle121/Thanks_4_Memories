e7d9b6da6e5ece43a39f63c22dee778d769c6d40
ckyle121 <carolinekyle21@gmail.com>
2022-08-28 15:30:26 -0500

    use moment style header of a post

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -3,14 +3,21 @@ import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import moment from 'moment';
 
 import useStyles from './styles.js';
 
-const Post = () => {
+const Post = ({ post }) => {
     const classes = useStyles();
     
     return (
-        <h1>POST</h1>
+        <Card className={classes.card}>
+            <CardMedia classname={classes.media} image={post.selectedFile} title={post.title}/>
+            <div className={classes.overlay}>
+                <Typography variant="h6">{post.creator}</Typography>
+                <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+            </div>
+        </Card>
     );   
 }
 
